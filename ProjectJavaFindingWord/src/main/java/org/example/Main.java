68c7c3d241d27d7e866e7d7fc1622ac81a342091
tinapetrovic15 <117905251+tinapetrovic15@users.noreply.github.com>
2022-11-15 11:57:43 +0100

    Add files via upload

--- ProjectJavaFindingWord/src/main/java/org/example/Main.java
+++ ProjectJavaFindingWord/src/main/java/org/example/Main.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner Scn = new Scanner(System.in);
-        String S = Scn.nextLine();
-        String word = Scn.nextLine();
-        System.out.println("The word balloon is contained "+Balloon.findingBalloon(S)+"  times in the word"+S);
+    public static void main(String[] args) throws IOException {
+        File file = new File("input.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String S = br.readLine();
+        String word = br.readLine();
+
+        System.out.println("The word BALLOON is contained "+Balloon.findingBalloon(S)+"  times in the word "+S);
         System.out.println("The word "+ word +" is contained "+findingWordByList.findingWord(S,word)+"  times in the word "+S);
         System.out.println("The word "+ word +" is contained "+findingWordByHashMap.findingWord(S,word)+"  times in the word "+S);
     }
